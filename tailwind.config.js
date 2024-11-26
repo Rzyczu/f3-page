b186476144d25d88143377bc653cb0be6208dbe5
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-11-26 20:41:35 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,31 +12,31 @@ module.exports = {
         '2xl': '6rem',
       },
     },
-      colors: {
-        transparent: 'transparent',
-        primary: {
-          light: '#B3A2CE',
-          DEFAULT: '#714C98',
-          dark: '#532984',
-        },
-        'white': '#FEFBF7',
-        'black': '#000000',
-        'gray': {light: '#C4C2C1'}
+    colors: {
+      transparent: 'transparent',
+      primary: {
+        light: '#B3A2CE',
+        DEFAULT: '#714C98',
+        dark: '#532984',
       },
-      fontFamily: {
-        'montserrat': ['Montserrat'],
+      'white': '#FEFBF7',
+      'black': '#000000',
+      'gray': { light: '#C4C2C1' }
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat'],
+    },
+    extend: {
+      gradientColorStopPositions: {
+        '1/3': '33.33333333333333333%',
+        '2/3': '66.6666666666666666666%',
+        '1/4': '25%',
+        '3/4': '75%'
       },
-      extend:{
-        gradientColorStopPositions: {
-          '1/3': '33.33333333333333333%',
-          '2/3': '66.6666666666666666666%',
-          '1/4': '25%',
-          '3/4': '75%'
-        },
-        translate: {
-          'half-logo': '45%', //44.744356936%
-        },
+      translate: {
+        'half-logo': '45%', //44.744356936%
       },
+    },
   },
   plugins: [],
 }
