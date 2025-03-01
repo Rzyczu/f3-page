e7a2def1cc81b809af5912f3ce1f7c33cdd32e30
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2025-03-01 11:02:38 +0100

    brrotherhood update wp
    (end added is first displayed)

--- wp/wp-content/themes/f3-page/inc/pages/about-us.php
+++ wp/wp-content/themes/f3-page/inc/pages/about-us.php
@@ -416,6 +416,76 @@ function board_member_meta_box($post) {
     <?php
 }
 
+// Dodajemy kolumnę "Group" w tabeli Board Members w panelu admina
+function add_board_member_group_column($columns) {
+    $columns['board_group'] = __('Group', 'your-theme-textdomain');
+    return $columns;
+}
+add_filter('manage_edit-board_member_columns', 'add_board_member_group_column');
+
+// Wypełniamy kolumnę "Group" odpowiednimi wartościami
+function fill_board_member_group_column($column, $post_id) {
+    if ($column === 'board_group') {
+        $group = get_post_meta($post_id, 'board_group', true);
+        if ($group === 'personnel') {
+            echo __('Kadra', 'your-theme-textdomain');
+        } elseif ($group === 'leaders') {
+            echo __('Drużynowi', 'your-theme-textdomain');
+        } elseif ($group === 'instructors') {
+            echo __('Instruktorzy', 'your-theme-textdomain');
+        } else {
+            echo __('N/A', 'your-theme-textdomain');
+        }
+    }
+}
+add_action('manage_board_member_posts_custom_column', 'fill_board_member_group_column', 10, 2);
+
+// Dodajemy filtr dla kolumny "Group" w widoku Board Members
+function filter_board_member_by_group($post_type) {
+    if ($post_type === 'board_member') {
+        $selected = isset($_GET['board_group_filter']) ? $_GET['board_group_filter'] : '';
+        ?>
+        <select name="board_group_filter">
+            <option value=""><?php _e('All Groups', 'your-theme-textdomain'); ?></option>
+            <option value="personnel" <?php selected($selected, 'personnel'); ?>><?php _e('Kadra', 'your-theme-textdomain'); ?></option>
+            <option value="leaders" <?php selected($selected, 'leaders'); ?>><?php _e('Drużynowi', 'your-theme-textdomain'); ?></option>
+            <option value="instructors" <?php selected($selected, 'instructors'); ?>><?php _e('Instruktorzy', 'your-theme-textdomain'); ?></option>
+        </select>
+        <?php
+    }
+}
+add_action('restrict_manage_posts', 'filter_board_member_by_group');
+
+// Modyfikacja zapytania admina, aby uwzględnić filtr Group
+function filter_board_member_query_by_group($query) {
+    global $pagenow;
+    if ($pagenow === 'edit.php' && isset($_GET['board_group_filter']) && $_GET['board_group_filter'] != '') {
+        $query->query_vars['meta_key'] = 'board_group';
+        $query->query_vars['meta_value'] = sanitize_text_field($_GET['board_group_filter']);
+    }
+}
+add_filter('parse_query', 'filter_board_member_query_by_group');
+
+// Ustawienie kolumny "Group" jako sortowalnej
+function make_board_member_group_column_sortable($columns) {
+    $columns['board_group'] = 'board_group';
+    return $columns;
+}
+add_filter('manage_edit-board_member_sortable_columns', 'make_board_member_group_column_sortable');
+
+// Modyfikacja zapytania w adminie, aby sortować po Group
+function sort_board_member_by_group($query) {
+    if (!is_admin() || !$query->is_main_query()) {
+        return;
+    }
+
+    if (isset($query->query_vars['orderby']) && $query->query_vars['orderby'] === 'board_group') {
+        $query->set('meta_key', 'board_group');
+        $query->set('orderby', 'meta_value'); // Sortowanie po wartości tekstowej
+    }
+}
+add_action('pre_get_posts', 'sort_board_member_by_group');
+
 
 function customize_section_brotherhood($wp_customize) {
     // Sekcja Customizera
